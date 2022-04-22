77b30f892f7cd929c8608acde44bc2ef17e13044
aigakira <aigazhyldyzbekova@gmail.com>
2022-04-22 13:23:59 +0200

    book list and book item ui added

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,9 +1,14 @@
-import { Box, Button, IconButton, List } from "@mui/material";
+import React, { useState } from "react";
+
+import { Box, Button, Divider, IconButton, Stack } from "@mui/material";
 import { Helmet } from "react-helmet-async";
+import "./index.css";
 
 import SortByAlphaIcon from "@mui/icons-material/SortByAlpha";
 import CollectionsBookmarkIcon from "@mui/icons-material/CollectionsBookmark";
 import AddBoxIcon from "@mui/icons-material/AddBox";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -12,8 +17,9 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 
 const Main = () => {
+  const [isFav, setIsFav] = useState(false);
   return (
-    <div style={{display:"flex",flexDirection:"column",gap:"50px"}}>
+    <div style={{ display: "flex", flexDirection: "column", gap: "50px" }}>
       <Helmet>
         <title>Main Page</title>
       </Helmet>
@@ -31,7 +37,7 @@ const Main = () => {
         </Button>
       </Box>
       <Box sx={{ display: "flex", gap: "20px" }}>
-        <FormControl sx={{minWidth: 250 }}>
+        <FormControl sx={{ minWidth: 250 }}>
           <InputLabel htmlFor="author-list">Author</InputLabel>
           <Select native defaultValue="" id="author-list" label="Author">
             <option aria-label="None" value="" />
@@ -39,7 +45,7 @@ const Main = () => {
             <option value={2}>Option 2</option>
           </Select>
         </FormControl>
-        <FormControl sx={{minWidth: 250 }}>
+        <FormControl sx={{ minWidth: 250 }}>
           <InputLabel htmlFor="categories-list">Categories</InputLabel>
           <Select defaultValue="" id="categories-list" label="Categories">
             <ListSubheader>Fictional</ListSubheader>
@@ -51,7 +57,31 @@ const Main = () => {
           </Select>
         </FormControl>
       </Box>
-      <List></List>
+      <section>
+        <div>
+          <h1 className="category_header">Business Education</h1>
+          <Divider />
+          <Stack direction="row" spacing={8} style={{ margin: "20px auto" }}>
+            <div>
+              <div className="img_div">
+                <img
+                  src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSFlx4x-DSoFn2AEBcxh-pURx2U7ECB4el5OztaFW3igFUXXDUK"
+                  alt="book_img"
+                />
+              </div>
+              {isFav ? (
+                <FavoriteIcon sx={{ color: "red", m: "5px 0 5px 0" }} onClick={()=>setIsFav(false)}/>
+              ) : (
+                <FavoriteBorderIcon sx={{ color: "red", m: "5px 0 5px 0" }} onClick={()=>setIsFav(true)}/>
+              )}
+              <div>
+                <p className="book_title">Title</p>
+                <p className="book_author">Author</p>
+              </div>
+            </div>
+          </Stack>
+        </div>
+      </section>
     </div>
   );
 };
